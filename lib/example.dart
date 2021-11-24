95a82fa22533f212ebf76fbe6d507a7823c89b1e
alphazen01 <parvejmahmud555@gmail.com>
2021-11-24 22:14:50 +0600

    TextFileAdded

---
+++ lib/example.dart
@@ -0,0 +1,14 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Example extends StatelessWidget {
+  const Example({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("example"),),
+    );
+  }
+}
